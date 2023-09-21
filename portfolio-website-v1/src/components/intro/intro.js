d935f953db3bb08bac0366b3a31ce1d2a4d9dba3
stevetang91 <steve.tang1991st@gmail.com>
2023-09-21 16:21:35 -0400

    add other navbars

---
+++ portfolio-website-v1/src/components/intro/intro.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function intro() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default intro
--- portfolio-website-v1/src/components/navbar/Navbar.js
+++ portfolio-website-v1/src/components/navbar/Navbar.js
@@ -7,8 +7,14 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
             <Link to='/' className='navbar-logo'>
-                Logo 
+                Logo <i class="fa-regular fa-snowflake"></i>
             </Link>
+            <Link to='/portfolio' className='navbar-portfolio'>
+                Portfolio
+            </Link>
+            <Link to='/about' className='navbar-about'>About</Link>
+            <Link to='/contact' className='navbar-contact'>Contact</Link>
+
         </div>
       </nav>
     </div>
